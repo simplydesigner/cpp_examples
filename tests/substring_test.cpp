8f05d278d91c015a02f9be080e2a45e50cbf75fa
alexey <alexey@me.com>
2016-10-10 16:14:19 +0300

    Catch

--- tests/substring_test.cpp
+++ tests/substring_test.cpp
@@ -1,8 +1,10 @@
 #include <catch.hpp>
 
-TEST_CASE("substring", "[cpp_examples][substring]") {
-    SECTION("brute_force") {
-    }
-    SECTION("kmp") {
+namespace ns_substring {
+    TEST_CASE("substring", "[cpp_examples][substring]") {
+        SECTION("brute_force") {
+        }
+        SECTION("kmp") {
+        }
     }
 }
