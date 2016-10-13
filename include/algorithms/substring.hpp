cc165bd520f5fcc1378e8a290478d09b399a4fd3
alexey <alexey@me.com>
2016-10-13 16:19:58 +0300

    O(...)

--- include/algorithms/substring.hpp
+++ include/algorithms/substring.hpp
@@ -3,16 +3,12 @@
 
 namespace ns_substring {
     namespace ns_brute_force {
-        /*  Алгоритм прямого поиска:
-            O(len(s) * len(p))
-        */
+        /* O(len(s) * len(p)) */
         const char* strstr(const char* s, const char* p);
     }
 
     namespace ns_kmp {
-        /*  Алгоритм Д. Кнута, Д. Мориса и В. Пратта (КМП-поиск):
-            O(len(s) + len(p))
-        */
+        /* O(len(s) + len(p)) */
         const char* strstr(const char* s, const char* p);
     }
 }
